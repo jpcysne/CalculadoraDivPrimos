9c6de3f8882acfa7c33ffdc6afd8cfcc55f86208
jp <joaopaulorubio@hotmail.com>
2021-11-28 16:43:33 -0300

    Adicionando Estrutura Inicial

---
+++ CalculadoraDivPrimos.Servicos/Interface/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDivPrimos.Servicos.Interface
+{
+    public interface ICalculator
+    {
+        List<int> CalcularDivisao(int numero);
+
+        List<int> CalcularPrimos(int numero);
+    }
+}
---
+++ CalculadoraDivPrimos.Servicos/Servico/CalculadoraServico.cs
@@ -0,0 +1,24 @@
+﻿using CalculadoraDivPrimos.Servicos.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDivPrimos.Servicos.Servico
+{
+    public class CalculadoraServico : ICalculator
+    {
+        public List<int> CalcularDivisao(int numero)
+        {
+            List<int> listaDivisao = new List<int>();
+            return listaDivisao;
+        }
+
+        public List<int> CalcularPrimos(int numero)
+        {
+            List<int> listaPrimos = new List<int>();
+            return listaPrimos;
+        }
+    }
+}
